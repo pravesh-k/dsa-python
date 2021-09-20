8d84a7ab6e618257a7fef6be737dcd190e032c86
Pravesh <praveshnayak@gmail.com>
2021-09-20 17:41:28 +0530

    optimized quick sort using in-place approach

--- search_sort/sort_quick.py
+++ search_sort/sort_quick.py
@@ -33,3 +33,39 @@ def quick_sort(arr):
 
 
 print("Sorted array is\n", quick_sort(arr))
+
+
+##  The above approach is not optimum as the auxiliary space required is O(n) due to usage of temporary sub arrays.
+##  That implied, above solution is not an in-place algorithm. An in-place solution is such solution where we don't 
+##  rely on temporary array and manipulation is done in the input array itself.
+#   The approach used below to implement quick sort uses O(log(n)) auxiliary space due to call stack.
+
+array = [64, 25, 12, 22, 11, 33]
+
+def partition(arr, low, high):
+
+    pivot = arr[high]
+
+    i = low-1
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+    # print("piv", i+1)
+    return i + 1
+
+def quick_sort_in_place(arr, low, high):
+
+    if low<high:
+        piv = partition(arr, low, high)
+        
+        # print("left", low, piv, high)
+        quick_sort_in_place(arr, low, piv-1)
+        # print("right", low, piv, high)
+        quick_sort_in_place(arr, piv+1, high)
+
+
+quick_sort_in_place(array, 0, len(array)-1)
+print("Sorted array using optimized quick sort\n", array)
