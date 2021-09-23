56dd2cc08763318e111a40127e78d1bbaefa649a
Pravesh <praveshnayak@gmail.com>
2021-09-23 23:21:40 +0530

    optimized: added iterative approach for same logic

--- traversal/bfs_tree.py
+++ traversal/bfs_tree.py
@@ -5,6 +5,7 @@
 #   Space complexity: O(n) where n is the number of nodes
 
 from queue import Queue
+import queue
 
 #creating node structure
 class Node:
@@ -14,8 +15,8 @@ class Node:
         self.left = None
         self.right = None
 
-#function for BFS using queue
-def breadth_first_search(root, q):
+#function for BFS using queue and recursive  approach
+def breadth_first_search_recursive(root, q):
 
     if q.empty():
         return
@@ -28,7 +29,28 @@ def breadth_first_search(root, q):
     if root.right != None:
         q.put(root.right)
     
-    breadth_first_search(root, q)   #recursive call to get child and find its children
+    breadth_first_search_recursive(root, q)   #recursive call to get child and find its children
+
+
+#function for BFS using queue and iterative approach
+def breadth_first_search_iterative(root):
+
+    if root is None:
+        return
+
+    q = []                          #using list for q implementation
+    q.append(root)                  #enqueuing/putting root of the tree to the queue
+
+    while(len(q) > 0):              #until queue is not empty, pop the first enqueued
+                                    #item and find the children and put into queue
+        print(q[0].data, end=" ")
+        node = q.pop(0)
+
+        if node.left is not None:
+            q.append(node.left)
+
+        if node.right is not None:
+            q.append(node.right)
 
 
 #Driver Program                     #       Tree Structure
@@ -44,4 +66,6 @@ root.right.right = Node(7)
 q = Queue(maxsize=0)    # initializing with infinite size limit
 
 q.put(root)
-breadth_first_search(root, q)
+breadth_first_search_recursive(root, q)
+print()
+breadth_first_search_iterative(root)
